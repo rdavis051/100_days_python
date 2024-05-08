ab4e92897acb14364b1850f874e79be4cb7a0c3e
Rob Davis <rdavis.systemsdesignpro@gmail.com>
2024-05-08 15:34:34 -0400

    adding day22 - Pong Game

---
+++ day22/pong_game/paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+LOCATION = (20, 100)
+
+class Paddle(Turtle):
+
+    def __init__(self, coordinates):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(coordinates)
+        self.speed("fastest")
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
