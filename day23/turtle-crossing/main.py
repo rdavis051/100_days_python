3d82861ce178d4c8d506ac63fe882f94d8e9104c
Rob Davis <rdavis.systemsdesignpro@gmail.com>
2024-05-18 11:14:55 -0400

    capestone - turtle crossing

---
+++ day23/turtle-crossing/main.py
@@ -0,0 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManger
+from scoreboard import Scoreboard
+
+# setting up display scren
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+# Initiating Objects
+player = Player()
+car_manager = CarManger()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=player.go_up, key="Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
