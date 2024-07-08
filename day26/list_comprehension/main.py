5a0503413f4e528b89a87f0303bc362cba56733b
Rob Davis <rdavis.systemsdesignpro@gmail.com>
2024-07-08 11:50:36 -0400

    adding exception handling to day26

--- day26/list_comprehension/main.py
+++ day26/list_comprehension/main.py
@@ -87,6 +87,14 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 #print(phonetic_dict)
 
-word = input("Please enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Please enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Input Invalid: Only Letters Please!")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
